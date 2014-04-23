cb07133f8bab9f464693343817d1a615cb2a232a
Jamie English <jamienglish@gmail.com>
2014-04-23 20:26:12 +0100

    Fixed news sql

--- src/goldfinchjewellery/models/news.clj
+++ src/goldfinchjewellery/models/news.clj
@@ -1,35 +1,37 @@
 (ns goldfinchjewellery.models.news
-  (:require [clojure.java.jdbc :as sql :refer [do-prepared insert-values
-                                               with-connection]]))
+  (:require [clojure.java.jdbc :as sql]))
 
 (def db {:classname "org.sqlite.JDBC"
          :subprotocol "sqlite"
          :subname "db.sq3"})
 
-(defn create-table []
-  (with-connection
+(def categories
+  ["Stockists" "Events & Exhibitions" "Awards" "Press"])
+
+(defn create-news-table []
+  (sql/with-connection
     db
-    (create-table :news
+    (sql/create-table :news
                       [:id "INTEGER PRIMARY KEY AUTOINCREMENT"]
                       [:created_at "TIMESTAMP DEFAULT CURRENT_TIMESTAMP"]
-                      [:category "TEXT"]
-                      [:content "TEXT"])))
+                      [:category "TEXT NOT NULL"]
+                      [:content "TEXT NOT NULL"])))
 
 (defn all []
-  (with-connection
+  (sql/with-connection
     db (sql/with-query-results res
          ["SELECT * FROM news ORDER BY category, created_at DESC"]
          (doall res))))
 
 (defn create [category content]
-  (with-connection
+  (sql/with-connection
     db
-    (insert-values
+    (sql/insert-values
       :news
       [:category :content :created_at]
       [category content (java.util.Date.)])))
 
 (defn delete [id]
-  (with-connection
+  (sql/with-connection
     db
-    (do-prepared "DELETE FROM news WHERE news.id = ?" [id])))
+    (sql/do-prepared "DELETE FROM news WHERE news.id = ?" [id])))
