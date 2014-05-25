00db9603dd9ce0bc4d0047392f1ee17479ea869e
Jamie English <jamienglish@gmail.com>
2014-05-25 18:32:13 +0100

    Dont use deprecated sql library

--- src/goldfinchjewellery/models/users.clj
+++ src/goldfinchjewellery/models/users.clj
@@ -1,28 +1,11 @@
 (ns goldfinchjewellery.models.users
-  (:require [clojure.java.jdbc.deprecated :as sql]
+  (:require [clojure.java.jdbc :as sql]
+            [goldfinchjewellery.models.migration :refer [db]]
             [noir.util.crypt :as crypt]))
 
-(def db {:classname "org.sqlite.JDBC"
-         :subprotocol "sqlite"
-         :subname "db.sq3"})
-
-(defn create-users-table []
-  (sql/with-connection db
-    (sql/create-table :users
-                  [:id "INTEGER PRIMARY KEY AUTOINCREMENT"]
-                  [:created_at "TIMESTAMP DEFAULT CURRENT_TIMESTAMP"]
-                  [:email "TEXT UNIQUE"]
-                  [:encrypted_password "TEXT"])))
-
 (defn create [email password]
-  (sql/with-connection db
-    (sql/insert-record
-      :users
-      {:email email
-       :encrypted_password (crypt/encrypt password)})))
+  (sql/insert!
+    db :users {:email email :encrypted_password (crypt/encrypt password)}))
 
 (defn find-by-email [email]
-  (sql/with-connection db
-    (sql/with-query-results res
-      ["SELECT * FROM users WHERE email = ?" email]
-      (first res))))
+  (first (sql/query db ["SELECT * FROM users WHERE email = ?" email])))
